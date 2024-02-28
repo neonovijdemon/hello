497564975fd8c3667eb8bb16b537e34ef169eb30
Afina <afinazarova@mail.ru>
2024-02-28 14:23:41 +0300

    commit 1-3

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -7,6 +7,9 @@ int main()
     cout << "Hello World!\n";
     cout << "my name is Max!\n";
     cout << "How are you?\n";
+    cout << "commit 1\n";
+    cout << "commit 2\n";
+    cout << "commit 3\n";
 }
 
 
